dd8dfd3fa4e335bb39e7085aef3624900c4de018
Mariia Makarkina <mariiamak25@gmail.com>
2023-02-04 19:46:14 +0200

    add useEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,95 @@
+import { useState, useEffect } from 'react';
+import { nanoid } from 'nanoid';
+
+import Form from './Form';
+import Contacts from './Contacts';
+import Filter from './Filter';
+import styles from '../components/phoneBook.module.css';
+
 export const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    const books = JSON.parse(localStorage.getItem('contacts'));
+    return books ? books : [];
+  });
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const isDublicate = name => {
+    const normalizedName = name.toLowerCase();
+    const contact = contacts.find(item => {
+      return item.name.toLowerCase() === normalizedName;
+    });
+
+    return Boolean(contact);
+  };
+
+  const formSubmitHandler = data => {
+    const { name, number } = data;
+    if (isDublicate(name, number)) {
+      return alert(
+        `Dear User, ${name} with number ${number} is already in your contacts!`
+      );
+    }
+
+    setContacts(prevState => {
+      const newContact = {
+        id: nanoid(3),
+        name,
+        number,
+      };
+      return [newContact, ...prevState];
+    });
+    return true;
+  };
+
+  const onDelete = id => {
+    setContacts(prevState => {
+      const newContacts = prevState.filter(user => user.id !== id);
+      return newContacts;
+    });
+  };
+
+  const onChangeFilter = event => {
+    const { value } = event.currentTarget;
+    setFilter(value);
+  };
+
+  const onFilter = () => {
+    const normalizedFilter = filter.toLowerCase();
+    const result = contacts.filter(({ name }) => {
+      return name.toLowerCase().includes(normalizedFilter);
+    });
+    return result;
+  };
+
+  // const componentDidMount = () => {
+  //   const contacts = JSON.parse(localStorage.getItem('contacts'));
+
+  //   if (contacts && contacts.length > 0) {
+  //     setContacts(contacts);
+  //   }
+  // };
+
+  // const componentDidUpdate = (prevProps, prevState) => {
+  //   if (contacts.length !== prevState.contacts.length)
+  //     localStorage.setItem('contacts', JSON.stringify(contacts));
+  // };
+
+  const isContacts = Boolean(contacts.length);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={styles.section}>
+      <h2 className={styles.title}>Phonebook</h2>
+      <Form onSubmit={formSubmitHandler} />
+      <div className={styles.blockContact}>
+        <h2 className={styles.title}>Contacts</h2>
+        <Filter onChangeFilter={onChangeFilter} />
+        {isContacts && <Contacts onFilter={onFilter} onDelete={onDelete} />}
+        {!isContacts && <p>There are no contacts in your Phone Book...</p>}
+      </div>
     </div>
   );
 };
